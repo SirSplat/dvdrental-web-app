c3145485452449222556a97c6752b4c5b95267f0
Leon Rogers <Leon.Rogers@BlueElephantDBA.co.uk>
2024-11-26 11:55:56 +0000

    Refactor some code and environment variables and start to implement fullstack containers.

--- db/initdb.sh
+++ db/initdb.sh
@@ -2,6 +2,20 @@
 set -e
 
 psql -v ON_ERROR_STP=1 --username "dbo" --dbname "dvdrental" <<-ESQL
+    CREATE SCHEMA dsa;
+    COMMENT ON SCHEMA dsa IS 'Home of all things ETL.';
 
+    CREATE SCHEMA rental;
+    COMMENT ON SCHEMA rental IS 'Home of all things application.';
+
+    CREATE ROLE pgrst_authenticator WITH LOGIN ENCRYPTED PASSWORD '${POSTGREST_DB_PASSWORD}';
+    GRANT USAGE ON SCHEMA rental TO pgrst_authenticator;
+    ALTER DEFULT PRIVILEGES IN SCHEMA rental GRANT SELECT ON TABLES TO pgrst_authenticator;
+    COMMENT ON ROLE pgrst_authenticator IS 'Role for PostgREST';
+
+    CREATE ROLE pgrst_anon WITH NOLOGIN;
+    GRANT USAGE ON SCHEMA rental TO pgrst_anon;
+    ALTER DEFULT PRIVILEGES IN SCHEMA rental GRANT SELECT ON TABLES TO pgrst_anon;
+    COMMENT ON ROLE pgrst_anon IS 'Role for PostgREST anonymous users';
 ESQL
 
