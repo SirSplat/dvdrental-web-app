2b18bf57ec4a4e279b46ffd1c840c1c0495f0639
Leon Rogers <Leon.Rogers@BlueElephantDBA.co.uk>
2024-11-27 09:29:46 +0000

    Setup application database to allow PostgREST to establish connections.

--- db/initdb.sh
+++ db/initdb.sh
@@ -8,14 +8,15 @@ psql -v ON_ERROR_STP=1 --username "dbo" --dbname "dvdrental" <<-ESQL
     CREATE SCHEMA rental;
     COMMENT ON SCHEMA rental IS 'Home of all things application.';
 
-    CREATE ROLE pgrst_authenticator WITH LOGIN ENCRYPTED PASSWORD '${POSTGREST_DB_PASSWORD}';
-    GRANT USAGE ON SCHEMA rental TO pgrst_authenticator;
-    ALTER DEFULT PRIVILEGES IN SCHEMA rental GRANT SELECT ON TABLES TO pgrst_authenticator;
+    CREATE ROLE pgrst_anon WITH NOLOGIN;
+    GRANT USAGE ON SCHEMA dsa, rental TO pgrst_anon;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA dsa, rental GRANT SELECT ON TABLES TO pgrst_anon;
+    COMMENT ON ROLE pgrst_anon IS 'Role for PostgREST anonymous users';
+
+    CREATE ROLE pgrst_authenticator WITH NOINHERIT LOGIN ENCRYPTED PASSWORD '${POSTGREST_DB_PASSWORD}';
+    GRANT CONNECT ON DATABASE dvdrental TO pgrst_authenticator;
     COMMENT ON ROLE pgrst_authenticator IS 'Role for PostgREST';
 
-    CREATE ROLE pgrst_anon WITH NOLOGIN;
-    GRANT USAGE ON SCHEMA rental TO pgrst_anon;
-    ALTER DEFULT PRIVILEGES IN SCHEMA rental GRANT SELECT ON TABLES TO pgrst_anon;
-    COMMENT ON ROLE pgrst_anon IS 'Role for PostgREST anonymous users';
+    GRANT pgrst_anon TO pgrst_authenticator;
 ESQL
 
